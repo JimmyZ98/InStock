48fd57643096786740ab97e5924f29a6efab94bf
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2022-06-13 16:55:23 -0400

    created routing paths in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage";
+import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage";
+import AddInventoryPage from "./pages/AddInventoryPage/AddInventoryPage";
+import EditInventoryPage from "./pages/EditInventoryPage/EditInventoryPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route path="/warehouse" component={WarehousePage} />
+        <Route path="/inventory" component={InventoryPage} />
+        <Route path="/warehouse-add" component={AddWarehousePage} />
+        <Route path="/warehouse-edit" component={EditWarehousePage} />
+        <Route path="/inventory-add" component={AddInventoryPage} />
+        <Route path="/inventory-edit" component={EditInventoryPage} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
