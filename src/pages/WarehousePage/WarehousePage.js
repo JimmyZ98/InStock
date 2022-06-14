584fe9e36dfe7de47dabed1301e892bfb7e34ac5
Liz Meeker <lizmeeker90@gmail.com>
2022-06-14 19:45:02 -0400

    created axios GET request

--- src/pages/WarehousePage/WarehousePage.js
+++ src/pages/WarehousePage/WarehousePage.js
@@ -1,12 +1,30 @@
 import { Component } from 'react';
 import "./WarehousePage.scss";
 import WarehouseListItem from '../../components/WarehouseListItem/WarehouseListItem';
+import axios from 'axios';
 
 class WarehousePage extends Component {
 
-  state = {  } 
+  state = { 
+    warehouseList: null
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/warehouses')
+      .then(response => {
+        this.setState({
+          warehouseList: response.data
+        })
+      })
+  };
 
   render() { 
+    const { warehouseList } = this.state;
+
+    if(!warehouseList) {
+      return <main>Loading, please wait...</main>
+    }
+
     return (
       <section className='warehouses'>
         <div className='warehouses__container'>
@@ -15,7 +33,14 @@ class WarehousePage extends Component {
           <button className='warehouses__button'>+Add New Warehouse</button>
         </div>
         <ul className='warehouses__list'>
-          <WarehouseListItem />
+          {
+            warehouseList.map(warehouse => (
+              <WarehouseListItem 
+                key={warehouse.id}
+                {...warehouse}
+              />
+            ))
+          }
         </ul>
       </section>
     );
