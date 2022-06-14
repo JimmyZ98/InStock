c4572c70baeaf6f2c5b98fc731eb1fe7f691e8f6
aryannaf <aryannafarhadi@gmail.com>
2022-06-13 23:38:57 -0400

    Created styled footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <div className="footer">
+        <p className="footer__copyright">&copy; InStock Inc. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
