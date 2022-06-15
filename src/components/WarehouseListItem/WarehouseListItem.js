da36d268b10ae6bca93f564bb7c04b2a364d80e9
Liz Meeker <lizmeeker90@gmail.com>
2022-06-14 21:22:41 -0400

    added mobile styling

--- src/components/WarehouseListItem/WarehouseListItem.js
+++ src/components/WarehouseListItem/WarehouseListItem.js
@@ -1,25 +1,35 @@
 import './WarehouseListItem.scss';
 import deleteIcon from '../../assets/Icons/delete_outline-24px.svg';
 import editIcon from '../../assets/Icons/edit-24px.svg';
+import chevronRight from '../../assets/Icons/chevron_right-24px.svg';
 
 const WarehouseListItem = ({ id, name, address, city, country, contactName, contactPhone, contactEmail }) => {
   return ( 
     <li className='warehouse'>
-      <div className='warehouse__info-container'>
-        <p className='warehouse__sub-header'>WAREHOUSE</p>
-        <p className='warehouse__info'>{name}</p>
-      </div>
-      <div className='warehouse__info-container'>
-        <p className='warehouse__sub-header'>CONTACT NAME</p>
-        <p className='warehouse__info'>{contactName}</p>
-      </div>
-      <div className='warehouse__info-container'>
-        <p className='warehouse__sub-header'>ADDRESS</p>
-        <p className='warehouse__info'>{address}, {city}, {country}</p>
-      </div>
-      <div className='warehouse__info-container'>
-        <p className='warehouse__sub-header'>CONTACT INFORMATION</p>
-        <p className='warehouse__info'>{contactPhone} {contactEmail}</p>
+      <div className='warehouse__container'>
+        <div className='warehouse__grouping'>
+          <div className='warehouse__info-container'>
+            <p className='warehouse__sub-header'>WAREHOUSE</p>
+            <div className='warehouse__link'>
+            <p className='warehouse__info warehouse__info--link'>{name}</p>
+            <img className='warehouse__icon' src={chevronRight} alt='link'/>
+            </div>
+          </div>
+          <div className='warehouse__info-container'>
+            <p className='warehouse__sub-header'>ADDRESS</p>
+            <p className='warehouse__info'>{address}, {city}, {country}</p>
+          </div>
+        </div>
+        <div className='warehouse__grouping'>
+          <div className='warehouse__info-container'>
+            <p className='warehouse__sub-header'>CONTACT NAME</p>
+            <p className='warehouse__info'>{contactName}</p>
+          </div>
+          <div className='warehouse__info-container'>
+            <p className='warehouse__sub-header'>CONTACT INFORMATION</p>
+            <p className='warehouse__info'>{contactPhone} {contactEmail}</p>
+          </div>
+        </div>
       </div>
       <div className='warehouse__icon-container'>
         <img className='warehouse__icon' src={deleteIcon} alt='delete'/>
