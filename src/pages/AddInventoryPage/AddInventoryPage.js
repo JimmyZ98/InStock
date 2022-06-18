62d6936219c8a0d5ab9adc269d3966292ff14282
Liz Meeker <lizmeeker90@gmail.com>
2022-06-17 21:00:46 -0400

    added event handlers

--- src/pages/AddInventoryPage/AddInventoryPage.js
+++ src/pages/AddInventoryPage/AddInventoryPage.js
@@ -3,30 +3,42 @@ import "./AddInventoryPage.scss";
 import backArrowIcon from '../../assets/Icons/arrow_back-24px.svg';
 import axios from 'axios';
 
-const server = 'http://localhost:8080';
+const API_URL = 'http://localhost:8080';
 class AddInventoryPage extends Component {
 
   state = { 
     quantityStyle: "new-inventory__quantity-container",
-    warehouseList: null
+    warehouseList: null,
+    itemName: localStorage.itemName || '',
+    description: localStorage.description || '',
+    quantity: localStorage.quantity || 0,
+    status: 'In Stock',
+    warehouseName: '',
+    category: '',
+
+    warehouseID: localStorage.warehouseId || ''
    }
 
    handleStatusChange = (e) => {
     if (e.target.value === "Out of Stock") {
       this.setState({
-        quantityStyle: "new-inventory__quantity-container new-inventory__quantity-container--display"
+        quantityStyle: "new-inventory__quantity-container new-inventory__quantity-container--display",
+        category: 'Out of Stock',
+        quantity: 0
       })
     }
     else if (e.target.value === "In Stock") {
       this.setState({
-        quantityStyle: "new-inventory__quantity-container"
+        quantityStyle: "new-inventory__quantity-container",
+        category: 'In Stock',
+        quantity: localStorage.quantity
       })
     };
    };
 
   populateWarehouseList = () => {
     let warehouseNames = [];
-    axios.get(`${server}/warehouses`)
+    axios.get(`${API_URL}/warehouses`)
     .then(response => {
       response.data.map(warehouse => {
         return warehouseNames.push(warehouse.name)
@@ -36,6 +48,39 @@ class AddInventoryPage extends Component {
       warehouseList: warehouseNames
     })
   };
+
+  handleNameChange = (e) => {
+    this.setState({
+      itemName: e.target.value
+    })
+    localStorage.itemName = e.target.value
+  };
+
+  handleDescriptionChange = (e) => {
+    this.setState({
+      description: e.target.value
+    })
+    localStorage.description = e.target.value
+  };
+
+  handleQuantityChange = (e) => {
+    this.setState({
+      quantity: e.target.value
+    })
+    localStorage.quantity = e.target.value
+  }
+
+  handleWarehouseChange = (e) => {
+    this.setState({
+      warehouseName: e.target.value
+    })
+  }
+
+  handleCategoryChange = (e) => {
+    this.setState({
+      category: e.target.value
+    })
+  }
 
    componentDidMount = () => {
     this.populateWarehouseList();
@@ -61,11 +106,28 @@ class AddInventoryPage extends Component {
             <div className="new-inventory__details">
               <h2 className="new-inventory__sub-title">Item Details</h2>
               <label className="new-inventory__label" htmlFor="name">Item Name</label>
-              <input className="new-inventory__input" id="name" placeholder="Item Name"/>
+              <input
+                className="new-inventory__input"
+                name="name"
+                placeholder="Item Name"
+                onChange={this.handleNameChange}
+                value={this.state.itemName}
+              />
               <label className="new-inventory__label" htmlFor="description">Description</label>
-              <input className="new-inventory__input new-inventory__input--description" id="description" placeholder="Please enter a brief item description..."/>
+              <input
+                className="new-inventory__input new-inventory__input--description"
+                name="description"
+                placeholder="Please enter a brief item description..."
+                onChange={this.handleDescriptionChange}
+                value={this.state.description}
+              />
               <label className="new-inventory__label" htmlFor="category">Category</label>
-              <select className="new-inventory__select" required id="category">
+              <select
+                className="new-inventory__select"
+                required
+                name="category"
+                onChange={this.handleCategoryChange}
+              >
                 <option value=''>Please select</option>
               </select>
             </div>
@@ -74,24 +136,51 @@ class AddInventoryPage extends Component {
               <p className="new-inventory__label">Status</p>
               <div className="new-inventory__radio-container">
                 <div className="new-inventory__radio">
-                  <input className='new-inventory__option' type="radio" id="in_stock" name="status" value="In Stock" defaultChecked onChange={this.handleStatusChange}/>
+                  <input
+                    className='new-inventory__option'
+                    type="radio" id="in_stock"
+                    name="status"
+                    value="In Stock"
+                    defaultChecked
+                    onChange={this.handleStatusChange}
+                  />
                   <label className='new-inventory__radio-text' htmlFor="in_stock">In stock</label>
                 </div>
                 <div className="new-inventory__radio">
-                  <input className='new-inventory__option' type="radio" id="out_of_stock" name="status" value="Out of Stock" onChange={this.handleStatusChange}/>
+                  <input
+                    className='new-inventory__option'
+                    type="radio"
+                    id="out_of_stock"
+                    name="status"
+                    value="Out of Stock"
+                    onChange={this.handleStatusChange}
+                  />
                   <label className='new-inventory__radio-text' htmlFor="out_of_stock">Out of stock</label>
                 </div>
               </div>
               <div className={this.state.quantityStyle}>
                 <label className="new-inventory__label" htmlFor="quantity">Quantity</label>
-                <input className="new-inventory__input" id="quantity" defaultValue='0'/>
+                <input
+                  className="new-inventory__input"
+                  name="quantity"
+                  onChange={this.handleQuantityChange}
+                  value={this.state.quantity}
+                />
               </div>
               <label className="new-inventory__label" htmlFor="warehouse">Warehouse</label>
-              <select className="new-inventory__select" required id="warehouse">
+              <select
+                className="new-inventory__select"
+                required
+                name="warehouse"
+                onChange={this.handleWarehouseChange}
+              >
                 <option value=''>Please select</option>
                 {
                   warehouseList.map(warehouse => (
-                    <option key={warehouse} value={warehouse}>
+                    <option
+                      key={warehouse}
+                      value={warehouse}
+                    >
                       {warehouse}
                     </option>
                   ))
